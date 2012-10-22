8de40ed8235fc986cc53dae1d982db20b306973e
Rich Boakes <rjb@port.ac.uk>
2012-10-22 15:29:18 +0100

    Added code to break up paths into hierarchical unordered lists.

--- dragon.js
+++ dragon.js
@@ -7,35 +7,90 @@ var rdfx = rdfx || {};
 rdfx.dragger = (function () {
 	'use strict';
 
-	var ensure_path, loaded;
+	var ensure_path_with_parent, ensure_path, paths_loaded, loaded;
 
+	// recursive function that takes a path_array (of strings) and
+	// ensures that each of its element strings exist as a chain of
+	// child elements below the parent element.  Elements that already
+	// exist are not replaced.
+	ensure_path_with_parent = function (prefix, path_array, parent) {
+		var id, elem, ul, li, span;
+
+		// take the head of the path_array
+		id = path_array.shift();
+
+		// when undefined, we've reached the end of the array
+		if (id) {
+			prefix += "_" + id;
+			// there is an id to be tested
+			elem = document.getElementById(prefix);
+			if (elem) {
+				// sometimes its already there
+				ensure_path_with_parent(prefix, path_array, elem);
+			} else {
+
+				// create a new list item element that is
+				// a child of the parent
+				li = document.createElement("li");
+				li.setAttribute("id", prefix + "_");
+
+				// create a span item that will go within the list
+				// and add necessary text to it.
+				span = document.createElement("span");
+				span.setAttribute("draggable", true);
+				span.appendChild(document.createTextNode(id));
+
+				// create an unordered list into which
+				// sublists may be added.
+				ul = document.createElement("ul");
+				ul.setAttribute("id", prefix);
+
+				// connect the new nodes to each other and to the
+				// parent element
+				parent.appendChild(li);
+				li.appendChild(span);
+				li.appendChild(ul);
+
+				// recurse
+				ensure_path_with_parent(prefix, path_array, ul);
+			}
+
+		}
+	};
+
+	// takes an array that represents a path, finds the root
+	// drag list element and kicks off a recursive
 	ensure_path = function (path_array) {
 		var ul = document.querySelector("#draglist");
-		ul.innerHTML += "<li>" + path_array + "</li>";
+		ensure_path_with_parent("rdfx", path_array,  ul);
+	};
+
+	paths_loaded = function (e) {
+		// break up the file into an array of lines
+		// by splitting it at every \n
+		var raw = this.response.split("\n");
+
+		// apply an anonymous function to each entry
+		// in the array
+		raw.map(
+			function (path) {
+				// split the line into an array such that
+				// books/web/design would become
+				// ["books","web","design"]
+				var parts = path.split("/");
+				ensure_path(parts);
+			}
+		);
 	};
 
 	// a function to be called when the document is loaded
 	loaded = function () {
-		console.log("doc loaded");
-
+		// Use an 'ajax' XMLHttpRequest2 object to
+		// load the db.txt file, and when this load is
+		// complete pass control to the paths_loaded function
 		var xhr = new XMLHttpRequest();
+		xhr.addEventListener("load", paths_loaded);
 		xhr.open('GET', 'db.txt', true);
-		xhr.onload = function (e) {
-
-			var out = [],
-				raw = this.response.split("\n");
-
-			raw.map(
-				function (path) {
-					var parts = path.split("/");
-					out.push(parts);
-					ensure_path(parts);
-				}
-			);
-
-			console.log("Retrieved", out);
-
-		};
 		xhr.send();
 	};
 
