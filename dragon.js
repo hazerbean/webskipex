ef7a74b2c8f4c6f12900caca0e1c9388fb49df1b
Rich Boakes <rjb@port.ac.uk>
2012-10-22 13:42:24 +0100

    Added ajax code to load file from server, and process it line by line (the processing isn't dont yet, just ariting the lines to a parent unordered list.

--- dragon.js
+++ dragon.js
@@ -7,9 +7,36 @@ var rdfx = rdfx || {};
 rdfx.dragger = (function () {
 	'use strict';
 
+	var ensure_path, loaded;
+
+	ensure_path = function (path_array) {
+		var ul = document.querySelector("#draglist");
+		ul.innerHTML += "<li>" + path_array + "</li>";
+	};
+
 	// a function to be called when the document is loaded
-	var loaded = function () {
+	loaded = function () {
 		console.log("doc loaded");
+
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', 'db.txt', true);
+		xhr.onload = function (e) {
+
+			var out = [],
+				raw = this.response.split("\n");
+
+			raw.map(
+				function (path) {
+					var parts = path.split("/");
+					out.push(parts);
+					ensure_path(parts);
+				}
+			);
+
+			console.log("Retrieved", out);
+
+		};
+		xhr.send();
 	};
 
 	// construct and return an object with a property called
